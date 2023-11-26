b2b86c34ed615f5325762e78f33bf438338b9dbc
tijsboussier <tijsboussier@gmail.com>
2023-11-26 09:09:49 +0100

    Trying to fix deployment issue

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,7 +11,7 @@ module.exports = {
       resolve: "gatsby-source-filesystem",
       options: {
         name: `blog`,
-        path: `${__dirname}/blog/`,
+        path: `${__dirname}/src/blog/`,
       }
     },
   ],
